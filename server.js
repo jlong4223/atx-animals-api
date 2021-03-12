05032343b5a7395ec85da8ba3666db8eae518c0f
Jared Long <jlong4223@gmail.com>
2021-03-12 12:27:36 -0600

    uses user routes in server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require("./config/db-config");
 // TODO import routes
 const homeRoute = require("./routes/welcome");
 const animalRoute = require("./routes/animals");
+const usersRoute = require("./routes/users");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +18,7 @@ app.use(cors());
 // TODO use routes
 app.use("/", homeRoute);
 app.use("/", animalRoute);
+app.use("/users", usersRoute);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
