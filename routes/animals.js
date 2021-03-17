f45963527b8a7412eb969c740e271b729a9a5c89
Jared Long <jlong4223@gmail.com>
2021-03-16 21:00:52 -0500

    checks for auth for crud routes

--- routes/animals.js
+++ routes/animals.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const animalCtrl = require("../controllers/animals");
-const { route } = require("./welcome");
 
 router.get("/animals", animalCtrl.showAnimals);
 router.get("/animals/:id", animalCtrl.showOneAnimal);
