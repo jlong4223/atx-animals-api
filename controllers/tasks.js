dd0e43a17aa4a2e2d5d507dec689e23a2cfc3a14
Jared Long <jlong4223@gmail.com>
2021-03-18 13:28:49 -0500

    adds edit, delete, and showbyID for tasks as well as logging user

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,6 +3,9 @@ const Task = require("../models/task");
 module.exports = {
   showTasks,
   createTask,
+  editTask,
+  deleteTask,
+  showTaskById,
   showPending,
   showOpenTasks,
   showCompleted,
@@ -18,6 +21,15 @@ async function showTasks(req, res) {
   res.json(tasks);
 }
 
+async function showTaskById(req, res) {
+  try {
+    const oneTask = await Task.findById(req.params.id);
+    res.json(oneTask);
+  } catch (err) {
+    res.json(err);
+  }
+}
+
 async function createTask(req, res) {
   try {
     await Task.create(req.body);
@@ -27,17 +39,40 @@ async function createTask(req, res) {
   }
 }
 
+async function editTask(req, res) {
+  try {
+    console.log(req.user);
+    const updateTheTask = await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json(updateTheTask);
+  } catch (err) {
+    res.json(err);
+  }
+}
+
+async function deleteTask(req, res) {
+  try {
+    console.log(req.user);
+    const deletedTask = await Task.findByIdAndDelete(req.params.id);
+    res.json(deletedTask);
+  } catch (err) {
+    res.json(err);
+  }
+}
+
 async function showOpenTasks(req, res) {
-  const task = await Task.find({ status: "" });
+  const task = await Task.find({ status: "open" });
+  //   console.log(req.user);
   res.json(task);
 }
 
 async function showPending(req, res) {
   const pendingTask = await Task.find({ status: "pending" });
+  //   console.log(req.user);
   res.json(pendingTask);
 }
 
 async function showCompleted(req, res) {
   const completedTask = await Task.find({ status: "completed" });
+  //   console.log(req.user);
   res.json(completedTask);
 }
